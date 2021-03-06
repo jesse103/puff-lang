99e7b4de9083eea23d39d395a0dcbff98bcaa2e7
Jesse <jesse@localhost.localdomain>
2021-03-06 07:30:41 -0500

    fix

--- src/attributes.c
+++ src/attributes.c
@@ -5,16 +5,9 @@ void setMainFunction(ast_t* ast)
     ast->isMainFunction = true;
 }
 
-void setName(ast_t* ast)
-{
-    ast->name = "yesse";
-}
-
 void appendAttribute(ast_t* ast, ast_t* attribute)
 {
     char* string_value = attribute->string_value;
     if(strcmp(string_value, "main_func") == 0)
         setMainFunction(ast);
-    if(strcmp(string_value, "test") == 0)
-        setName(ast);
 }
